81c9d16e2c4b1060fcb467c7812792fc7f20c966
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2011-02-03 14:50:39 +0100

    hudson

---
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -0,0 +1,16 @@
+require_recipe "apt" 
+
+execute "jenkins-key" do                                                                                                                            
+  command "wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -"                                                                                     
+  action :run                                                                                                                                                
+end
+
+execute "update-sources" do
+  command "echo \"deb http://pkg.jenkins-ci.org/debian binary/\" >>/etc/apt/sources.list"
+  action :run
+end
+
+# Some neat package (subversion is needed for "subversion" chef ressource)
+%w{ debconf subversion mc htop curl jenkins }.each do |a_package|
+  package a_package
+end
