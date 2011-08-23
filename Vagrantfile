fcea7a1e034bcb704f6ff4595cd037e9e6a3d97b
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2011-08-23 08:54:05 +0200

    New chef version

--- Vagrantfile
+++ Vagrantfile
@@ -34,8 +34,6 @@ Vagrant::Config.run do |config|
 	 chef.add_recipe("vagrant_main") 
   end
 
-  config.vm.provision :shell, :path => "php-hudson.sh"
-
   # Enable provisioning with chef server, specifying the chef server URL,
   # and the path to the validation key (relative to this Vagrantfile).
   #
--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -1,23 +1,16 @@
 require_recipe "apt" 
 require_recipe "git"
+require_recipe "chef-jenkins:default"
 
-execute "jenkins-key" do                                                                                                                            
-  command "wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -"                                                                                     
-  action :run                                                                                                                                                
-end
 
-execute "update-sources" do
-  command "echo \"deb http://pkg.jenkins-ci.org/debian binary/\" >>/etc/apt/sources.list"
-  action :run
-end
 
-execute "apt-update" do
-  command "sudo apt-get update"
-  action :run
-end
+# %w(git URLSCM build-publisher).each do |plugin|
+#   jenkins_cli "install-plugin #{plugin}"
+#   jenkins_cli "safe-restart"
+# end
 
 # Some neat package (subversion is needed for "subversion" chef ressource)
-%w{ debconf ant subversion mc htop curl jenkins php5-xdebug php5-cli php-pear gawk}.each do |a_package|
+%w{ debconf ant subversion mc htop curl php5-xdebug php5-cli php-pear gawk}.each do |a_package|
   package a_package
 end
 
